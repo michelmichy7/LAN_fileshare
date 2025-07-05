586752cea70dc24bd776e1d8fdbec94d7b60c9ba
Michal Lysak <michelmichy7@gmail.com>
2025-07-05 16:24:33 +0200

    Test

--- Backend/UDP_searchAround.cpp
+++ Backend/UDP_searchAround.cpp
@@ -16,7 +16,6 @@ Backend::Backend(QObject *parent)
 void Backend::sendPacket() {
     if (!senderSocket) {
         senderSocket = new QUdpSocket(this);
-        connect(senderSocket, &QUdpSocket::readyRead, this, &Backend::onSReadyRead);
 
         senderSocket->bind(QHostAddress::AnyIPv4, 0, QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint);
         senderSocket->writeDatagram(QByteArray("FIND_DEVICE"), QHostAddress::Broadcast, 45454);
@@ -27,7 +26,7 @@ void Backend::sendPacket() {
 void Backend::catchPacket() {
     if (!catcherSocket) {
         catcherSocket = new QUdpSocket(this);
-        connect(catcherSocket, &QUdpSocket::readyRead, this, &Backend::onCReadyRead);
+        connect(catcherSocket, &QUdpSocket::readyRead, this, &Backend::onReadyRead);
         bool success = catcherSocket->bind(QHostAddress::AnyIPv4, 45454, QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint);
 
         if (!success) {
@@ -38,44 +37,8 @@ void Backend::catchPacket() {
 
 }
 
-void Backend::onCReadyRead()
-{
 
-    while (catcherSocket->hasPendingDatagrams()) {
-        qDebug() << "Socket state:" << catcherSocket->state();
-        qDebug() << "Socket bound to:" << catcherSocket->localPort();
-        QHostAddress senderIP;
-        QByteArray datagram;
-        datagram.resize(catcherSocket->pendingDatagramSize());
-        quint16 senderPort;
-
-        catcherSocket->readDatagram(datagram.data(), datagram.size(), &senderIP, &senderPort);
-
-        if (datagram == "FIND_DEVICE") {
-            qDebug() << "Found a Device";
-            QString rawIP = senderIP.toString();
-
-            if (rawIP.startsWith("::ffff:")) {
-                rawIP = rawIP.mid(7);
-            }
-            m_model->addItem(rawIP);
-
-            qDebug() << "Current model size:" << m_model->rowCount();
-            QStringList allItems = m_model->stringList();
-
-
-            for (const QString &item : std::as_const(allItems)) {
-                qDebug() << item;
-            }
-
-        } else if (datagram == "CONNECT_REQUEST"){
-            qDebug() << "Received connection request:" << senderIP;
-            emit showConnectionPage();
-        }
-        }
-    }
-
-void Backend::onSReadyRead()
+void Backend::onReadyRead()
 {
 
     while (senderSocket->hasPendingDatagrams()) {
